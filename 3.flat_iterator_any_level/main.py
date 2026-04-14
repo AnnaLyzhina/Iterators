fcffcfc09b82b89cbc2b7b09ad9dc72ab00cf317
AnnaLyzhina <ann.lyzhina@gmail.com>
2026-04-14 14:52:39 +0300

    main.py

--- 3.flat_iterator_any_level/main.py
+++ 3.flat_iterator_any_level/main.py
@@ -1,1 +1,25 @@
+class FlatIterator:
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+        self.flat_list = []
+        self.index = 0
 
+    def flatten(self, items):
+        for item in items:
+            if isinstance(item, list):
+                self.flatten(item)
+            else:
+                self.flat_list.append(item)
+
+    def __iter__(self):
+        self.flat_list = []
+        self.index = 0
+        self.flatten(self.list_of_list)
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.flat_list):
+            raise StopIteration
+        item = self.flat_list[self.index]
+        self.index += 1
+        return item
