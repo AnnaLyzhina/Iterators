3e486bd9bdad004f08e5d03a8e266a709c6abdb1
AnnaLyzhina <ann.lyzhina@gmail.com>
2026-04-15 10:35:46 +0300

    main.py

--- 3.flat_iterator_any_level/main.py
+++ 3.flat_iterator_any_level/main.py
@@ -23,3 +23,22 @@ class FlatIterator:
         item = self.flat_list[self.index]
         self.index += 1
         return item
+
+def test_3():
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == [
+        'a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!'
+    ]
+
+if __name__ == '__main__':
+    test_3()
